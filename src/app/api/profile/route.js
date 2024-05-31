55633652b2f514db0588b8405319252d773f35ef
Joe Bumbulis <joebumbulis@gmail.com>
2024-05-31 14:10:42 -0500

    creates logic to get kv using userid from header

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -1,14 +1,18 @@
+import { getRequestContext } from '@cloudflare/next-on-pages';
+
 export const runtime = 'edge'
 
 export async function GET(request) {
 
   const userId = request.headers.get('user-id');
+  const myKv = getRequestContext().env.kv_user_IDs;
+  const kvValue = await myKv.get(userId) || false;
 
-  if (!userId) {
-    return new Response('User ID not present in headers', { status: 400 });
+  if (!userId || !kvValue) {
+    return new Response('Missig or Incorrect UserId', { status: 400 });
   }
 
-  return new ReportingObserver(`User ID: ${userId}`, {
+  return new Response(`User ID: ${kvValue}`, {
     headers: {'Content-Type': 'text/plain'},
   });
 }
