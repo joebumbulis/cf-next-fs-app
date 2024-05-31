41b05de16553228aac2665bf066b56d4632441ba
Joe Bumbulis <joebumbulis@gmail.com>
2024-05-31 15:35:57 -0500

    adds fetch to origin with auth token

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -7,12 +7,22 @@ export async function GET(request) {
   const userId = request.headers.get('user-id');
   const myKv = getRequestContext().env.kv_user_IDs;
   const kvValue = await myKv.get(userId) || false;
+  const origin = "www.joebumbulis.com"
 
   if (!userId || !kvValue) {
     return new Response('Missig or Incorrect UserId', { status: 400 });
   }
 
-  return new Response(`User ID: ${kvValue}`, {
-    headers: {'Content-Type': 'text/plain'},
-  });
+  return await fetch(origin, {
+    headers: {
+      "Auth-Token": kvValue,
+    }
+  })
+
+  // let newRequest = new Request(request);
+  // newRequest.headers.set("Auth-Token", kvValue)
+  // return new Response()
+  // return new Response(`User ID: ${kvValue}`, {
+  //   headers: {'Content-Type': 'text/plain'},
+  // });
 }
